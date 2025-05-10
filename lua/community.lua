3e98bc29c9bb0fe36512bd48bf888d17872bc1b9
Matthias Allner <allner.matthias@gmail.com>
2025-05-10 15:51:45 +0200

    added rust community pack

--- lua/community.lua
+++ lua/community.lua
@@ -8,6 +8,7 @@ return {
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.java" },
   { import = "astrocommunity.pack.ps1" },
+  { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.motion.mini-move" },
   { import = "astrocommunity.recipes.picker-lsp-mappings" }, -- snacks.picker for lsp requests (references ...)
   { import = "astrocommunity.recipes.diagnostic-virtual-lines-current-line" },
