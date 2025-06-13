65cadf6b4651b58654dae890f7147794a68ba479
Matthias Allner <Allner.Matthias@gmail.com>
2025-06-14 01:58:14 +0200

    added auto-reload of unmodified buffers when their source files change in the background

--- lua/polish.lua
+++ lua/polish.lua
@@ -3,3 +3,20 @@
 -- fit in the normal config locations above can go here
 
 -- TODO: update which terminal is used in <leader-t-f/h/v...>
+
+-- have buffers auto-refresh when underlying file changes (noticed at certain events)
+-- TODO: implement a toggle for this, if it becomes to slow or messaging becomes annoying or I need actual tailing behaviour
+vim.opt.autoread = true
+vim.opt.updatetime = 2000
+
+vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "FocusGained" }, {
+  command = "checktime",
+  desc = "Auto‑reload files changed on disk",
+})
+
+vim.api.nvim_create_autocmd("FileChangedShellPost", {
+  pattern = "*",
+  callback = function()
+    vim.notify("File changed on disk. Buffer reloaded.", vim.log.levels.WARN, { title = "AutoReload" })
+  end,
+})
