d97d52c5747280f002a0f4fbeed030550bd3e934
matthias allner <matthias.allner@fiatec.de>
2026-06-16 19:31:02 +0200

    bumped AstroNvim to v6 (tested only on windows)

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,11 +2,20 @@
 ---@type LazySpec
 return {
   "nvim-treesitter/nvim-treesitter",
+  lazy = false,
+  branch = "main",
+  build = ":TSUpdate",
+  init = function() require("nvim-treesitter.install").compilers = { "clang", "zig" } end,
   opts = {
-    ensure_installed = {
-      "lua",
-      "vimdoc",
-      -- add more arguments for adding more treesitter parsers
+    treesitter = {
+      highlight = true, -- enable/disable treesitter based highlighting
+      indent = true, -- enable/disable treesitter based indentation
+      auto_install = true, -- enable/disable automatic installation of detected languages
+      ensure_installed = {
+        "lua",
+        "vim",
+        -- add more arguments for adding more treesitter parsers
+      },
     },
   },
 }
